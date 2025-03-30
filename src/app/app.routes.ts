92070a3fa49d984c33821836ae8145eabff43327
rdgr1 <rodgerdias6@gmail.com>
2025-03-30 15:23:53 -0300

    feat: create navbar component with responsivity for devices mobile

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { InicioComponent } from './pages/inicio/inicio.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo:'inicio', pathMatch: 'full'},
+    {
+        path: 'inicio',
+        component: InicioComponent,
+        pathMatch: 'full'
+    }
+];
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { Component, Inject, PLATFORM_ID } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   imports: [],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+  styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
+menuOpen = false;
+isBrowser = false;
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
 
+  ngOnInit():void {
+    this.isBrowser = isPlatformBrowser(this.platformId);
+    if (this.isBrowser){
+       console.log('Rodando no browser')
+    }
+  }
+  toggleMenu(): void {
+    this.menuOpen = !this.menuOpen;
+  }
 }
