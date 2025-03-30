a9a600bd1d91829bf052700acb6262d82c023a59
rdgr1 <rodgerdias6@gmail.com>
2025-03-30 12:46:32 -0300

    feat: firts commit

---
+++ src/app/pages/inicio/inicio.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { NavbarComponent } from "../../components/navbar/navbar.component";
+
+@Component({
+  selector: 'app-inicio',
+  imports: [NavbarComponent],
+  templateUrl: './inicio.component.html',
+  styleUrl: './inicio.component.scss'
+})
+export class InicioComponent {
+
+}
