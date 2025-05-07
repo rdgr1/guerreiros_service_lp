b3a8fff1fa77934cfde8981a20effa25b1c3f516
rdgr1 <rodgerdias6@gmail.com>
2025-05-06 22:05:17 -0300

    feat: fix routes

--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -1,4 +1,28 @@
-import { Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { ModuleWithProviders, NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
 
 export const routes: Routes = [
+  {
+    path: '',
+    component: AppComponent,
+    pathMatch: 'full',
+    data: { title: 'App' }
+  },
+  {
+    path: '**',
+    redirectTo: '',
+    pathMatch: 'full'
+  }
 ];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, {
+      scrollPositionRestoration: 'enabled', // restaura scroll ao navegar
+      anchorScrolling: 'enabled'            // ativa âncoras (#servicos etc.)
+    })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
