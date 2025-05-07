a1fa7b78c44a1a0d47de0dc8cb5ecf95f67885e4
rdgr1 <rodgerdias6@gmail.com>
2025-05-07 20:23:27 -0300

    fix]

--- front-end/src/app/pages/inicio/inicio.component.ts
+++ front-end/src/app/pages/inicio/inicio.component.ts
@@ -1,13 +1,28 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from "../../components/navbar/navbar.component";
 import { ButtonSecondComponent } from "../../components/button-second/button-second.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-inicio',
-  imports: [ButtonSecondComponent],
+  imports: [ButtonSecondComponent,CommonModule],
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.scss'
 })
 export class InicioComponent {
+  videoStarted = false;
+  scrollToForm() {
+    const formElement = document.querySelector('#formulario');
+    if (formElement) {
+      formElement.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
+  playVideo(video: HTMLVideoElement) {
+    video.play();
+    this.videoStarted = true;
+  }
 
+  onPlay() {
+    this.videoStarted = true;
+  }
 }
